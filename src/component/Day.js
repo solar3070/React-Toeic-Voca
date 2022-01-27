202c0dfe0c3b3e2412c90c8f218653ab6da5a626
HyeJun <solar3070@naver.com>
2022-01-27 21:24:14 +0900

    lesson14 PUT, DELETE

--- src/component/Day.js
+++ src/component/Day.js
@@ -1,5 +1,6 @@
 import { useParams } from "react-router-dom";
 import Word from "./Word";
+import useFetch from "../hooks/useFetch";
 
 export default function Day() {
   const { day } = useParams();
--- src/component/Word.js
+++ src/component/Word.js
@@ -1,6 +1,8 @@
 import { useState } from "react";
 
-export default function Word({ word }) {
+export default function Word({ word: w }) {
+  // 혹은 props
+  const [word, setWord] = useState(w); // props.word
   const [isShow, setIsShow] = useState(false);
   const [isDone, setIsDone] = useState(word.isDone);
 
@@ -9,7 +11,36 @@ export default function Word({ word }) {
   }
 
   function toggleDone() {
-    setIsDone(!isDone);
+    fetch(`http://localhost:3001/words/${word.id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        ...word,
+        isDone: !isDone,
+      }),
+    }).then((res) => {
+      if (res.ok) {
+        setIsDone(!isDone);
+      }
+    });
+  }
+
+  function del() {
+    if (window.confirm("삭제 하시겠습니까?")) {
+      fetch(`http://localhost:3001/words/${word.id}`, {
+        method: "DELETE",
+      }).then((res) => {
+        if (res.ok) {
+          setWord({ id: 0 });
+        }
+      });
+    }
+  }
+
+  if (word.id === 0) {
+    return null;
   }
 
   return (
@@ -21,7 +52,9 @@ export default function Word({ word }) {
       <td>{isShow && word.kor}</td>
       <td>
         <button onClick={toggleShow}>뜻 {isShow ? "숨기기" : "보기"}</button>
-        <button className="btn_del">삭제</button>
+        <button onClick={del} className="btn_del">
+          삭제
+        </button>
       </td>
     </tr>
   );
